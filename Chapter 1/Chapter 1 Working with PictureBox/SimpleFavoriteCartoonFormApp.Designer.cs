0cf7bc8688081f5d3c935513f87e255d477dc27c
Jhanna Rose Hangad <jhannarosehangad82@gmail.com>
2025-03-24 21:00:55 +0800

    Add files via upload

---
+++ Chapter 1/Chapter 1 Working with PictureBox/SimpleFavoriteCartoonFormApp.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SimpleFavoriteCartoonFormApp
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.comboBox = new System.Windows.Forms.ComboBox();
+			this.viewBtn = new System.Windows.Forms.Button();
+			this.clearBtn = new System.Windows.Forms.Button();
+			this.pictureBox = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.BackColor = System.Drawing.Color.Transparent;
+			this.label1.Font = new System.Drawing.Font("Britannic Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(54, 38);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(460, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "SIMPLE FAVORITE CARTOON CHARACTER PICKER";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.BackColor = System.Drawing.Color.Transparent;
+			this.label2.Font = new System.Drawing.Font("Britannic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(84, 124);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(132, 21);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Select an item:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.Color.Transparent;
+			this.label3.Font = new System.Drawing.Font("Britannic Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(413, 124);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(61, 21);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Image";
+			// 
+			// comboBox
+			// 
+			this.comboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.comboBox.FormattingEnabled = true;
+			this.comboBox.Location = new System.Drawing.Point(88, 158);
+			this.comboBox.Name = "comboBox";
+			this.comboBox.Size = new System.Drawing.Size(186, 24);
+			this.comboBox.TabIndex = 3;
+			this.comboBox.Text = "Spongebob";
+			// 
+			// viewBtn
+			// 
+			this.viewBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("viewBtn.BackgroundImage")));
+			this.viewBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.viewBtn.Location = new System.Drawing.Point(88, 192);
+			this.viewBtn.Name = "viewBtn";
+			this.viewBtn.Size = new System.Drawing.Size(186, 31);
+			this.viewBtn.TabIndex = 4;
+			this.viewBtn.Text = "View";
+			this.viewBtn.UseVisualStyleBackColor = true;
+			this.viewBtn.Click += new System.EventHandler(this.viewBtn_Click);
+			// 
+			// clearBtn
+			// 
+			this.clearBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("clearBtn.BackgroundImage")));
+			this.clearBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.clearBtn.Location = new System.Drawing.Point(88, 229);
+			this.clearBtn.Name = "clearBtn";
+			this.clearBtn.Size = new System.Drawing.Size(186, 32);
+			this.clearBtn.TabIndex = 5;
+			this.clearBtn.Text = "Clear";
+			this.clearBtn.UseVisualStyleBackColor = true;
+			this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+			// 
+			// pictureBox
+			// 
+			this.pictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox.BackgroundImage")));
+			this.pictureBox.Location = new System.Drawing.Point(417, 158);
+			this.pictureBox.Name = "pictureBox";
+			this.pictureBox.Size = new System.Drawing.Size(316, 185);
+			this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox.TabIndex = 6;
+			this.pictureBox.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.Ivory;
+			this.panel1.Location = new System.Drawing.Point(331, 98);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(10, 294);
+			this.panel1.TabIndex = 7;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.pictureBox);
+			this.Controls.Add(this.clearBtn);
+			this.Controls.Add(this.viewBtn);
+			this.Controls.Add(this.comboBox);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox comboBox;
+		private System.Windows.Forms.Button viewBtn;
+		private System.Windows.Forms.Button clearBtn;
+		private System.Windows.Forms.PictureBox pictureBox;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
+
---
+++ Chapter 1/Chapter 1 Working with PictureBox/SimpleFavoriteCartoonFormApp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace SimpleFavoriteCartoonFormApp
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+			string[] cartoonChar = { "Spongebob", "Patrick", "Mr. Krabs", "Sandy" };
+			comboBox.Items.AddRange(cartoonChar);
+
+			comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+			comboBox.SelectedIndex = 0;
+		}
+
+		private void viewBtn_Click(object sender, EventArgs e)
+		{
+
+			string selectedItem = comboBox.SelectedItem.ToString();
+
+			switch (selectedItem)
+			{
+				case "Spongebob":
+					pictureBox.Image = Image.FromFile(@"C:/Users/Aljhon/OneDrive/Pictures/spongebob.jpg");
+					break;
+
+				case "Patrick":
+					pictureBox.Image = Image.FromFile(@"C:/Users/Aljhon/OneDrive/Pictures/patrick.jpg");
+					break;
+
+				case "Mr. Krabs":
+					pictureBox.Image = Image.FromFile(@"C:/Users/Aljhon/OneDrive/Pictures/krab.jpg");
+					break;
+
+				case "Sandy":
+					pictureBox.Image = Image.FromFile(@"C:/Users/Aljhon/OneDrive/Pictures/sandy.jpg");
+					break;
+
+				default:
+					pictureBox.Image = null;
+					break;
+			}
+		}
+
+		private void clearBtn_Click(object sender, EventArgs e)
+		{
+			pictureBox.Image = null;
+		}
+	}
+	}
+
